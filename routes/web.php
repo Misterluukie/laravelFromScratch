a20c6530527a81833e457c1cea0446930f6603ee
Luuk van der Plas <plas.van.der.luuk@gmail.com>
2021-06-14 21:55:46 +0200

    gekke merge door switch van localhost

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,9 @@
 <?php
 
-<<<<<<< HEAD
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\Rules\Exists;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
-=======
-use App\Models\Post as ModelsPost;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\Rules\Exists;
->>>>>>> 3802954a82ccef0790f67a2ea42c0a217ff2b4a1
 
 /*
 |--------------------------------------------------------------------------
@@ -23,7 +17,6 @@ use Illuminate\Validation\Rules\Exists;
 */
 
 Route::get('/', function () {
-<<<<<<< HEAD
 
     // return view('home', [
     //     'posts' => Post::all()
@@ -35,35 +28,12 @@ Route::get('/', function () {
 
     ddd($document);
 
-=======
-    return view('home');
->>>>>>> 3802954a82ccef0790f67a2ea42c0a217ff2b4a1
 });
 
 Route::get('/posts/{post}', function ($slug) {
 
-<<<<<<< HEAD
     return view('post', [
         'post' => Post::find($slug)
     ]);
 
-=======
-    $post = ModelsPost::find($slug);
-
-    return view("post", [
-        "post" -> $post
-    ]);
-
-    // if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-    //     // abort(404);
-    //     return redirect('/');
-    // }
-
-    // $post = cache()->remember("posts.{$slug}", 5, function () use ($path) {
-    //     var_dump('hoi');
-    //     return file_get_contents($path);
-    // });
-
-    // return view('post', ['post' => $post]);
->>>>>>> 3802954a82ccef0790f67a2ea42c0a217ff2b4a1
 })->where('post', '[A-z_\-]+');
